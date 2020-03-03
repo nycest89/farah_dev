853bef3a80372733cb1a265c9af788e1bf67b6cf
farahim <farahim@farahim.com>
2020-03-03 15:21:44 +0800

    farah

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="jumbotron text-center">
-    <h1>Hello! Welcome to Farah Project!</h1>
+    <h1>Hello All! Welcome to Farah Project!</h1>
     <p>Hi this is my new laravel development site.</p>
     <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
 </div>
