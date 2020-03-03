1b4ca7cc36cc5efc311d21fb067bc1f783ef415e
farahim <farahim@farahim.com>
2020-03-03 13:03:16 +0800

    farah

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 <h1>Blog Posts</h1>
+<div class="card-body">
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+             {{ session('status') }}
+         </div>
+      @endif
+        <form class="form-inline my-2 my-lg-0">
+             <button class="btn btn-sm btn-outline-secondary" type="button"><a href="/posts/create">Create New Post</a></button>
+        </form>  
+</div>
+<hr>
 @if (count($posts)>0)
     @foreach ($posts as $post)
         <div class='card p-3 mt-3 mb-3'>
